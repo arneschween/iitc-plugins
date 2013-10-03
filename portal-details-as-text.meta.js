4a7b489d989d24cb44335144a447b016283c33d9
arneschween <arne@schween.de>
2013-10-03 22:50:21 +0200

    Create portal-details-as-text.meta.js

---
+++ portal-details-as-text.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-portal-details-as-text@arneschween
+// @name           IITC plugin: display detailed portal information as text
+// @category       info
+// @version        0.1.0.20130920.214800
+// @namespace      https://github.com/arneschween/ingress-intel-total-conversion
+// @updateURL      https://github.com/arneschween/iitc-plugins/raw/master/portal-details-as-text.meta.js
+// @downloadURL    https://github.com/arneschween/iitc-plugins/raw/master/portal-details-as-text.user.js
+// @description    [arneschween-2013-09-20-214800] displays detailed portal information as text
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*https://github.com/arneschween/iitc-plugins/raw/master/portal-details-as-text.user.js
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
