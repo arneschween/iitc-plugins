c75bf8e6b7f8a94e12b41db547ae3878766d21dc
arneschween <arne@schween.de>
2014-01-07 02:40:54 +0100

    Update export-import-draw.user.js

--- export-import-draw.user.js
+++ export-import-draw.user.js
@@ -4,6 +4,8 @@
 // @version        0.0.2
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @description    Import/Export Data from draw tools
+// @updateURL https://github.com/arneschween/iitc-plugins/raw/master/export-import-draw.meta.js
+// @downloadURL https://github.com/arneschween/iitc-plugins/raw/master/export-import-draw.user.js
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
