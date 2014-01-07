6876e21c9845bc2031f8314a3ec045427a73796c
arneschween <arne@schween.de>
2014-01-07 02:41:34 +0100

    Create export-import-draw.meta.js

---
+++ export-import-draw.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id             iitc-export-import-draw@arneschween
+// @name           IITC plugin: export and import data from draw tools
+// @version        0.0.2
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @description    Import/Export Data from draw tools
+// @updateURL https://github.com/arneschween/iitc-plugins/raw/master/export-import-draw.meta.js
+// @downloadURL https://github.com/arneschween/iitc-plugins/raw/master/export-import-draw.user.js
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// ==/UserScript==
